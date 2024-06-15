1fb5da627a9d999edece684437e8aaf23b689fe6
allenkay76 <allenkay76@protonmail.com>
2024-06-15 21:24:16 +1000

    ADDED: AF_Renderer.h

---
+++ include/AF_Renderer.h
@@ -0,0 +1,11 @@
+#ifndef AF_RENDERER_H
+#define AF_RENDERER_H
+
+void AFLIB_InitRenderer(void);
+
+void AFLIB_DisplayRenderer(void);
+
+void AFLIB_DestroyRenderer(void);
+
+#endif // AF_RENDERER_H
+
