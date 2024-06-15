3702eac59f1d9eafb49491924a55433d768bd8ef
allenkay76 <allenkay76@protonmail.com>
2024-06-15 21:26:44 +1000

    EDIT: updated main to support new windowing

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include "AF_Window.h"
+#include "AF_Renderer.h"
+
+
 
 // Forward declare of c function
 int main(void);
@@ -7,8 +10,26 @@ int main(void);
 int main(void) {
     printf("ENTRY POINT: AF_Lib\n");
 
-    // Create the window
-    AF_Lib_CreateWindow();
+    
+   // Create the window
+    AF_Window gameWindow;
+    AFLib_CreateWindow(&gameWindow);
+
+    // Create the renderer
+    AFLIB_InitRenderer();
+
+    // update the window
+    while(AFLib_UpdateWindow(&gameWindow)){
+
+        // Display the renderer
+        AFLIB_DisplayRenderer();
+    }
+
+    // Destroy the renderer
+    AFLIB_DestroyRenderer();
+
+    // Destroy the window
+    AFLib_TerminateWindow(&gameWindow);
     return 0;
 }
 
