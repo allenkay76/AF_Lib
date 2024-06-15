651f7d9fe954d48847f00f8ebf6e47931101e5f4
allenkay76 <allenkay76@protonmail.com>
2024-06-15 21:21:35 +1000

    EDIT: added bool type

--- include/AF_Lib_Define.h
+++ include/AF_Lib_Define.h
@@ -1,8 +1,14 @@
 #ifndef AF_LIB_DEFINE_H
 #define AF_LIB_DEFINE_H
 
+// Define Static
 #define internal static
 #define local_persist static
 #define global_variable static
+
+// Define Bool
+#define TRUE 1
+#define FALSE 0
+typedef int bool;
 
 #endif
