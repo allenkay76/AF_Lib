a90c219851805443f268429f6933ac7687b3e318
allenkay76 <allenkay76@protonmail.com>
2024-06-15 21:25:36 +1000

    ADDED: opengl renderer implementation file

---
+++ src/common/AF_OpenGL_Renderer.c
@@ -0,0 +1,17 @@
+#include "AF_Renderer.h"
+#include <stdio.h>
+
+// Implementations
+void AFLIB_InitRenderer(void){
+    printf("OpenGL Renderer Initialized\n");
+
+}
+
+
+void AFLIB_DisplayRenderer(void){
+    //printf("OpenGL Display\n");
+}
+
+void AFLIB_DestroyRenderer(void){
+    printf("OpenGL Renderer Destroyed\n");
+}
