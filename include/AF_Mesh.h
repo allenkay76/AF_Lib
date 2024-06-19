02893015602167b7e523a9e88ce996b05b415d7d
allenkay76 <allenkay76@protonmail.com>
2024-06-19 21:42:06 +1000

    EDIT: loaded vertices and indices into struct from .obj file

--- include/AF_Mesh.h
+++ include/AF_Mesh.h
@@ -66,8 +66,10 @@ inline AF_Mesh AF_Mesh_GetOBJAttribSize(FILE* _file){
         // num of "vt" texture coordsfound
         // num of "vn" normal vectors found
     char fileBuffer[1024] ; // Buffer for reading the file
+    
     // read the file
     while(!feof(_file)){
+        
          if(fgets(fileBuffer, 1024, _file) != NULL) {
         // print the return value (aka string read in) to terminal
             AF_Vertex vertex;
@@ -75,13 +77,13 @@ inline AF_Mesh AF_Mesh_GetOBJAttribSize(FILE* _file){
                 // vertex
                 AF_Vec3 vertPos = AF_Mesh_GetVec3FromString(fileBuffer);
                 vertex.position = vertPos;
-                AF_Log("v %f %f %f\n", vertex.position.x, vertex.position.y, vertex.position.z);
                 mesh.vertexCount++;
                 continue;
             }
             if(fileBuffer[0] == 'f'){
                 // face / indices
-                mesh.indices ++;
+                // add 4 faces to the index count
+                mesh.indexCount+=4;
                 continue;
             }
             if(fileBuffer[0] == 'v' && fileBuffer[1] == 't'){
@@ -102,9 +104,91 @@ inline AF_Mesh AF_Mesh_GetOBJAttribSize(FILE* _file){
             }
         }
     }
-    
+    // Move back to the beginning of the file using fseek and rewind
+    fseek(_file, 0L, SEEK_SET);
+    rewind(_file);
     return mesh;
 }
+
+inline int AF_Mesh_Load_Data(FILE* _file, AF_Mesh* _mesh){
+    int verticesCount = 0;
+    int indicesCount = 0;
+    char fileBuffer[1024] ; // Buffer for reading the file
+    while(!feof(_file)){
+         if(fgets(fileBuffer, 1024, _file) != NULL) {
+        // print the return value (aka string read in) to terminal
+            AF_Vertex vertex;
+            if(fileBuffer[0] == 'v' && fileBuffer[1] == ' '){
+                // vertex
+                AF_Vec3 vertPos = AF_Mesh_GetVec3FromString(fileBuffer);
+                vertex.position = vertPos;
+                AF_Log("v %f %f %f\n", vertex.position.x, vertex.position.y, vertex.position.z);
+
+                _mesh->vertices[verticesCount] = vertex;
+                verticesCount++;
+                continue;
+            }
+            if(fileBuffer[0] == 'f'){
+                // face / indices
+                // vertex
+                // get the first element of each space sperated token
+                char* token = strtok(fileBuffer, " ");
+                int index = 0;
+                if(token != NULL){
+                    // face 1
+                    token = strtok(NULL, " ");
+                    index = strtof(token, NULL);
+                    _mesh->indices[indicesCount] = index;
+                    indicesCount++;
+                    AF_Log("i %i\n", index);
+
+                    // face 2
+                    token = strtok(NULL, " ");
+                    index = strtof(token, NULL);
+                    _mesh->indices[indicesCount] = index;
+                    indicesCount++;
+                    AF_Log("i %i\n", index);
+
+                    // face 3
+                    token = strtok(NULL, " ");
+                    index = strtof(token, NULL);
+                    _mesh->indices[indicesCount] = index;
+                    indicesCount++;
+                    AF_Log("i %i\n", index);
+
+                    // face 4
+                    token = strtok(NULL, " ");
+                    index = strtof(token, NULL);
+                    _mesh->indices[indicesCount] = index;
+                    indicesCount++;
+                    AF_Log("i %i\n", index);
+                }
+                
+
+                
+                continue;
+            }
+            if(fileBuffer[0] == 'v' && fileBuffer[1] == 't'){
+                // texture coordinate
+                //AF_Vec2 texPos = AF_Mesh_GetVec2FromString(fileBuffer);
+                //vertex.texCoord = texPos;
+                //AF_Log("vt %f %f\n", vertex.texCoord.x, vertex.texCoord.y);
+                //mesh.vertexCount++;
+                continue;
+            }
+            if(fileBuffer[0] == 'v' && fileBuffer[1] == 'n'){
+                // normal
+                //AF_Vec3 normPos = AF_Mesh_GetVec3FromString(fileBuffer);
+                //vertex.normal = normPos;
+                //AF_Log("vn %f %f %f\n", vertex.normal.x, vertex.normal.y, vertex.normal.z);
+                //mesh.vertexCount++;
+                continue;
+            }
+        }
+    }
+    return 0;
+}
+
 
 inline static int AF_Mesh_Load_OBJ(const char* _filePath, AF_Mesh* _mesh){
     if(_mesh == NULL){
@@ -127,7 +211,14 @@ inline static int AF_Mesh_Load_OBJ(const char* _filePath, AF_Mesh* _mesh){
 
     // create indices array of size indexCount
     mesh.indices = (int*)malloc(sizeof(int) * mesh.indexCount);
-    
+    AF_Mesh_Load_Data(file, &mesh);
+    for(int i = 0; i < mesh.vertexCount; i++){
+        AF_Log("V %f %f %f\n", mesh.vertices[i].position.x, mesh.vertices[i].position.y, mesh.vertices[i].position.z);
+    }
+
+    for(int i = 0; i < mesh.indexCount; i++){
+        AF_Log("I %i\n", mesh.indices[i]);
+    }
 
     AF_Log("Mesh Verticies %d\n", mesh.vertexCount);
     AF_Log("Mesh Indices %d\n", mesh.indexCount);
