377da6adedcd28502c55e6b3781a9c25f4efca07
allenkay76 <allenkay76@protonmail.com>
2024-07-23 08:20:05 +1000

    ADDED: material struct

---
+++ include/AF_Material.h
@@ -0,0 +1,19 @@
+/*
+===============================================================================
+AF_MATERIAL_H defninitions
+
+Logging helper functions for the game
+Calls vfprintf but adds some colour to text output
+===============================================================================
+*/
+
+#ifndef AF_MATERIAL_H
+#define AF_MATERIAL_H
+
+typedef struct{
+	uint32_t shaderID;
+	uint32_t textureID;
+} AF_Material;
+
+#endif
+
